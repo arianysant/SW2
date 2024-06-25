fc11858c18efb82ade7ac979d04d0486d64dbc9f
Ariany Andrade <100931444+arianysant@users.noreply.github.com>
2024-06-25 11:05:46 -0300

    Add files via upload

---
+++ aula_api/api/data.php
@@ -0,0 +1,47 @@
+<?php
+
+return [
+    [
+        'nome' => 'Ariany',
+        'email' => 'ary@gmail' 
+    ],
+    [
+        'nome' => 'Thifany',
+        'email' => 'thifany@gmail' 
+    ],
+    [
+        'nome' => 'Daniel',
+        'email' => 'daniel@gmail' 
+    ],
+    [
+        'nome' => 'Ana Clara',
+        'email' => 'ana@gmail' 
+    ],
+    [
+        'nome' => 'Isadora',
+        'email' => 'isadora@gmail' 
+    ],
+    [
+        'nome' => 'Stephanie',
+        'email' => 'tete@gmail' 
+    ],
+    [
+        'nome' => 'Ana Laura',
+        'email' => 'ana.laura@gmail' 
+    ],
+    [
+        'nome' => 'Rolim',
+        'email' => 'rolim@gmail' 
+    ],
+    [
+        'nome' => 'Maria Rita',
+        'email' => 'maria.rita@gmail' 
+    ],
+    [
+        'nome' => 'Igor',
+        'email' => 'iguinho@gmail' 
+    ],
+
+];
+
+?>
